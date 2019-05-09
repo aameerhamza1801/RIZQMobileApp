8e2ecc2e3e5ba76e9651428baeabe061e2429a1f
Ameer Hamza <aameerhamza1801@gmail.com>
2019-05-10 02:48:06 +0500

    Added Animation

--- app/src/main/java/com/example/talha/rizq/MainActivity.java
+++ app/src/main/java/com/example/talha/rizq/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.talha.rizq;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -7,7 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.talha.rizq.Model.Users;
@@ -24,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Button register, login;
     private ProgressDialog loadingBar;
+    private ObjectAnimator animRotationY;
+    private ImageView logo;
+    private TextView tag1,tag2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +45,31 @@ public class MainActivity extends AppCompatActivity {
 
         register = (Button) findViewById(R.id.btn_main_register);
         login = (Button) findViewById(R.id.btn_main_login);
+        logo = (ImageView) findViewById(R.id.iv_logo);
+        tag1 = (TextView) findViewById(R.id.tv_tag1);
+        tag2 = (TextView) findViewById(R.id.tv_tag2);
+
         loadingBar = new ProgressDialog(this);
         Paper.init(this);
+
+        //Animation rotation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotate);
+        //rotation.setFillAfter(true);
+        //logo.startAnimation(rotation);
+
+        //  Rotation Animation
+
+        ObjectAnimator animation = ObjectAnimator.ofFloat(logo, "rotationY", 0.0f, 360f);
+        animation.setDuration(3600);
+        animation.setRepeatCount(ObjectAnimator.INFINITE);
+        animation.setInterpolator(new AccelerateDecelerateInterpolator());
+        animation.start();
+
+        // Translation Animation
+
+        translateanimation(280,-200,0,0,3000,tag1);
+        translateanimation(-150,230,0,0,3000,tag2);
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +99,22 @@ public class MainActivity extends AppCompatActivity {
                 loadingBar.show();
             }
         }
+    }
+
+    private void translateanimation(int fromx,int tox,int fromy,int toy,int dur,View v){
+        TranslateAnimation cloud_moving = new TranslateAnimation(
+                Animation.ABSOLUTE, fromx,
+                Animation.ABSOLUTE, tox,
+                Animation.ABSOLUTE, fromy,
+                Animation.ABSOLUTE, toy
+        );
+
+        cloud_moving.setDuration(dur);
+        cloud_moving.setFillAfter(true);
+        cloud_moving.setStartOffset(1000);
+        cloud_moving.setRepeatCount(Animation.INFINITE);
+        cloud_moving.setRepeatMode(Animation.REVERSE);
+        v.startAnimation(cloud_moving);
     }
 
     private void directLoginAccess(final String uname, final String pass) {
