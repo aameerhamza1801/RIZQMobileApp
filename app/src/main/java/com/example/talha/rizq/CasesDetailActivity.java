30a241c0bb6e48697e5bc8c9c3c487846d4854b8
Ameer Hamza <aameerhamza1801@gmail.com>
2019-05-15 16:50:24 +0500

    Design improvements(Case Detail,paypal detail)

--- app/src/main/java/com/example/talha/rizq/CasesDetailActivity.java
+++ app/src/main/java/com/example/talha/rizq/CasesDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.talha.rizq;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,13 @@ import android.widget.Toast;
 import com.example.talha.rizq.Model.Cases;
 import com.example.talha.rizq.Model.Events;
 import com.example.talha.rizq.Prevalent.Prevalent;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -24,12 +32,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+
 public class CasesDetailActivity extends AppCompatActivity {
 
     private TextView cd_name, cd_description, cd_cnic, cd_account, cd_amount;
     private ImageView cd_image;
     private Button donate;
     private String cid = "";
+    private PieChart pieChart;
 
 
     @Override
@@ -46,6 +57,7 @@ public class CasesDetailActivity extends AppCompatActivity {
         cd_amount = (TextView)findViewById(R.id.user_case_detail_amount);
         cd_image = (ImageView)findViewById(R.id.user_case_detail_image);
         donate = (Button) findViewById(R.id.donate);
+        pieChart = (PieChart) findViewById(R.id.case_amount_pie_chart);
 
 
         getCasesDetails(cid);
@@ -103,12 +115,14 @@ public class CasesDetailActivity extends AppCompatActivity {
                 if(dataSnapshot.exists()){
                     Cases cases = dataSnapshot.getValue(Cases.class);
 
-                    cd_description.setText(cases.getNeedy_name());
+                    cd_name.setText(cases.getNeedy_name());
                     cd_description.setText(cases.getDescription());
                     cd_cnic.setText(cases.getCnic());
                     cd_account.setText(cases.getAccount());
                     cd_amount.setText("Amount :  "+cases.getCollected_amount()+"  /  "+cases.getNeeded_amount());
                     Picasso.get().load(cases.getImage()).into(cd_image);
+
+                    setChart(cases.getCollected_amount(),cases.getNeeded_amount());
                 }
             }
 
@@ -117,5 +131,62 @@ public class CasesDetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void setChart(String collected_amount, String needed_amount) {
+
+        int col_amt = Integer.parseInt(collected_amount);
+        int ned_amt = Integer.parseInt(needed_amount);
+        System.out.println("Collected: "+col_amt);
+        System.out.println("Remaining: "+ned_amt);
+        System.out.println("Division Test: "+(int)Math.ceil((double)col_amt/ned_amt*100));
+
+
+
+        int per = (int)Math.ceil((double)col_amt/ned_amt*100);
+        int per_neg = 100-per;
+
+        System.out.println("Coll %: "+per);
+        System.out.println("Rem %: "+per_neg);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.99f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(65f);
+
+        pieChart.setCenterText(collected_amount+" / "+needed_amount);
+        pieChart.setCenterTextSize(15);
+        pieChart.setCenterTextColor(Color.MAGENTA);
+
+
+        ArrayList<PieEntry> vals = new ArrayList<>();
+
+        vals.add(new PieEntry(per,"Amount Collected"));
+        vals.add(new PieEntry(per_neg,"Amount Remaining"));
+
+        Description description = new Description();
+        description.setText("Amount Collected ( % )");
+        description.setTextSize(15);
+        description.setPosition(500,540);
+        pieChart.setDescription(description);
+
+        pieChart.animateY(1000,Easing.EaseInOutCubic);
+        pieChart.setEntryLabelColor(Color.BLACK);
+
+        PieDataSet dataSet = new PieDataSet(vals," (%)");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(15f);
+        data.setValueTextColor(Color.RED);
+
+        pieChart.setData(data);
     }
 }
